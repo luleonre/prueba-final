6ecb3d8a478fc5ec0838cef0858309477362a5f6
luleonre <luleon@unal.edu.co>
2026-02-25 17:12:53 -0500

    Se finalizo la calculadora

--- app.js
+++ app.js
@@ -2,3 +2,17 @@ function magia(){
     let nombre = document.getElementById("nombre-usuario").value
     document.getElementById("titulo-principal").innerText = "Felicidades "+nombre+ "!"
 }
+
+function agregarNumero(numero){
+    document.getElementById("display").innerText += numero;
+}
+
+function limpiar(){
+    document.getElementById("display").innerText = "";
+}
+
+function calcular() {
+    let operacion = document.getElementById("display").innerText;
+    let resultado = eval(operacion);
+    document.getElementById("display").innerText = resultado;
+}
